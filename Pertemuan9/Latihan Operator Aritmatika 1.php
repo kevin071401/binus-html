24b77bcd2d4994bd8a11bd62c4b2c64dcbe5a7d7
kevin071401 <kevinjuliua@gmail.com>
2025-06-12 19:44:46 +0700

    Create Latihan Operator Aritmatika 1.php

---
+++ Pertemuan9/Latihan Operator Aritmatika 1.php
@@ -0,0 +1,11 @@
+<?php
+$L = readline("Lebar: ");
+$P = readline("Panjang: ");
+$T = readline("Tinggi: ");
+$V = ($L * $P * $T);
+
+echo("$L Meter");echo("\n");
+echo("$P Meter");echo("\n");
+echo("$T Meter");echo("\n");
+echo("$V Meter Kubik");echo("\n");
+?>
