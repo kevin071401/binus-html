2826179a75d5a9d4119e56081b0b8ad765c46877
kevin071401 <kevinjuliua@gmail.com>
2025-06-12 20:04:12 +0700

    Create Latihan Operator Aritmatika 3.php

---
+++ Pertemuan9/Latihan Operator Aritmatika 3.php
@@ -0,0 +1,10 @@
+<?php
+$r = readline("Jari-Jari: ");
+$T = readline("Tinggi: ");
+const PI = 3.14;
+$V = (PI * ($r ** 2) * $T);
+
+echo("$r Meter");echo("\n");
+echo("$T Meter");echo("\n");
+echo("$V Meter Kubik");echo("\n");
+?>
