d0b8544690ade54594b20dcad278fa913c6d3ae2
kevin071401 <kevinjuliua@gmail.com>
2025-06-12 20:18:15 +0700

    Create Create Latihan Operator Aritmatika 4.php

---
+++ Pertemuan9/Create Latihan Operator Aritmatika 4.php
@@ -0,0 +1,11 @@
+<?php
+$A = readline("Nilai A = ");
+$B = readline("Nilai B = ");
+$C = readline("Nilai C = ");
+$D = (($B ** 2) - 4 * $A * $C);
+
+echo("$A");echo("\n");
+echo("$B");echo("\n");
+echo("$C");echo("\n");
+echo("Nilai Diskriminan = $D");echo("\n");
+?>
