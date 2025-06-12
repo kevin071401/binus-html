3ad48ad548280bdd6da3c31e8d988b312f428137
kevin071401 <kevinjuliua@gmail.com>
2025-06-12 19:28:11 +0700

    Create Latihan Operator Aritmatika 1.js

---
+++ Pertemuan9/Latihan Operator Aritmatika 1.js
@@ -0,0 +1,9 @@
+let L = parseInt(prompt("lebar= ")) ;
+let P = parseInt(prompt("Panjang= ")) ;
+let T = parseInt(prompt("Tinggi= ")) ;
+
+
+console.log(L + " Meter");
+console.log(P + " Meter");
+console.log(T + " Meter");
+console.log(L * P * T + " Meter Kubik")
