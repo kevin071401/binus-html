3803798afb1d79eefded158661c81d0bc7b7a2b2
kevin071401 <kevinjuliua@gmail.com>
2025-06-12 20:14:33 +0700

    Create Latihan Operator Aritmatika 4.js

---
+++ Pertemuan9/Latihan Operator Aritmatika 4.js
@@ -0,0 +1,9 @@
+let A = parseInt(prompt("Nilai A = ")) ;
+let B = parseInt(prompt("Nilai B = ")) ;
+let C = parseInt(prompt("NIlai C = ")) ;
+
+
+console.log(A);
+console.log(B);
+console.log(C);
+console.log("Nilai Diskrimian = " + ((B ** 2) - 4 * A * C) )
