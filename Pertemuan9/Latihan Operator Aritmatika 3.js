dddc9998238f27f487b12193bab0b836391fe1af
kevin071401 <kevinjuliua@gmail.com>
2025-06-12 20:01:28 +0700

    Create Latihan Operator Aritmatika 3.js

---
+++ Pertemuan9/Latihan Operator Aritmatika 3.js
@@ -0,0 +1,7 @@
+let r = parseInt(prompt("Jari-Jari= ")) ;
+let T = parseInt(prompt("Tinggi= ")) ;
+const PI = 3.14;
+
+console.log(r + " Meter");
+console.log(T + " Meter");
+console.log(PI * (r ** 2) * T + " Meter Kubik")
