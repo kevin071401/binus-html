dfdec495cc63fe8fcbb9e20ad903e07119304aba
kevin071401 <kevinjuliua@gmail.com>
2025-06-24 13:26:41 +0700

    Update Latihan Perulangan 1 while.php

--- pertemuan11/Latihan Perulangan 1 while.php
+++ pertemuan11/Latihan Perulangan 1 while.php
@@ -1,8 +1,8 @@
 <?php
 $choice = "y";
+$angka = readline("Contoh Angka ");
 
 while (true) {
-    $angka = readline("Contoh Angka ");
     if ($choice == "y") {
         echo($angka);echo("\n");
         if ($angka % 2 == 0) {
@@ -11,8 +11,8 @@ while (true) {
             echo("angka $angka adalah bilangan ganjil");echo("\n");
         }
     }else {
-        echo("Program Berhenti");echo("\n");
-        echo("Terima kasih telah menggunakan program saya");echo("\n");
+        echo("Program Berhenti\n");
+        echo("Terima kasih telah menggunakan program saya\n");
         break;
     }
     $choice = readline("Ingin mengulanginya lagi? ");
