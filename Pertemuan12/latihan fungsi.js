6b10a77fb1566d14c31a7e198b0c1e5aeb9c0576
kevin071401 <kevinjuliua@gmail.com>
2025-06-24 20:36:07 +0700

    Update latihan fungsi.js

--- Pertemuan12/latihan fungsi.js
+++ Pertemuan12/latihan fungsi.js
@@ -28,44 +28,34 @@ inijudul()
 console.log()
 let menu = prompt("Masukkan Menu(+|-|/|*|%|stop): ");
 if (menu == "+") {
-    inijudul()
-    console.log()
     let nilai1 = parseInt(prompt("nilai 1= "));
     let nilai2 = parseInt(prompt("nilai 2= "));
     fg_int_tambah(nilai1, nilai2)
-    console.log(fg_int_tambah(nilai1, nilai2));
+    console.log("Hasil tambah", nilai1, "+", nilai2, "adalah", fg_int_tambah(nilai1, nilai2));
     let menu = prompt("Masukkan Menu(+|-|/|*|%|stop): ");
 } else if (menu == "-") {
-    inijudul()
-    console.log()
     let nilai1 = parseInt(prompt("nilai 1= "));
     let nilai2 = parseInt(prompt("nilai 2= "));
     fg_int_kurang(nilai1, nilai2)
-    console.log(fg_int_kurang(nilai1, nilai2));
+    console.log("Hasil kurang", nilai1, "-", nilai2, "adalah", fg_int_kurang(nilai1, nilai2));
     let menu = prompt("Masukkan Menu(+|-|/|*|%|stop): ");
 } else if (menu == "/") {
-    inijudul()
-    console.log()
     let nilai1 = parseInt(prompt("nilai 1= "));
     let nilai2 = parseInt(prompt("nilai 2= "));
     fg_int_bagi(nilai1, nilai2)
-    console.log(fg_int_bagi(nilai1, nilai2));
+    console.log("Hasil bagi", nilai1, "/", nilai2, "adalah", fg_int_bagi(nilai1, nilai2));
     let menu = prompt("Masukkan Menu(+|-|/|*|%|stop): ");
 } else if (menu == "*") {
-    inijudul()
-    console.log()
     let nilai1 = parseInt(prompt("nilai 1= "));
     let nilai2 = parseInt(prompt("nilai 2= "));
     fg_int_kali(nilai1, nilai2)
-    console.log(fg_int_kali(nilai1, nilai2));
+    console.log("Hasil kali", nilai1, "*", nilai2, "adalah", fg_int_kali(nilai1, nilai2));
     let menu = prompt("Masukkan Menu(+|-|/|*|%|stop): ");
 } else if (menu == "%") {
-    inijudul()
-    console.log()
     let nilai1 = parseInt(prompt("nilai 1= "));
     let nilai2 = parseInt(prompt("nilai 2= "));
     fg_int_sisabagi(nilai1, nilai2)
-    console.log(fg_int_sisabagi(nilai1, nilai2));
+    console.log("Hasil sisabagi", nilai1, "%", nilai2, "adalah", fg_int_sisabagi(nilai1, nilai2));
     let menu = prompt("Masukkan Menu(+|-|/|*|%|stop): ");
 } else {
     console.log("Program Berhenti\nTerima kasih telah menggunakan program saya");
